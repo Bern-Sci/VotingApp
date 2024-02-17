73413d571c345dc02b5b9d4787a41474d4b612d2
Bern-Sci <116250478+Bern-Sci@users.noreply.github.com>
2024-02-17 22:32:03 +0800

    Added ChangePositionInPos function

--- VotingApp/Vote.cs
+++ VotingApp/Vote.cs
@@ -96,6 +96,53 @@ public class Vote
     }
 
 
+    public void changeVoteInThisPos(Position position)
+    {
+        List<int> indexOfPos = new List<int>();
+        //kuhaon ang index sa candidate nga ang position kay equal sa position parameter
+        for (int i = 0; i < chosenCandidate.Count; i++)
+            if (chosenCandidate[i].pos.Equals(position))
+            {
+                indexOfPos.Add(i);
+                chosenCandidate[i] = null; //diko sure og diba ni mag memory leak. ichange nalang basta dapat ma null value
+            }
+        //Para ng babaw maedit ang value sa index diha sa chosenCandidate
+        int index = 0; //index ctr sa indexOfPos
+
+        List<Candidate> samePosCandidate = candidateList.getCandidatesInPos(position);
+        int remainingVotes = indexOfPos.Count;
+        while (remainingVotes != 0)
+        {
+            Console.Clear();
+            Console.WriteLine($"Position: {position.ToString()} - Remaining Votes: {remainingVotes}");
+            Console.WriteLine("Choose a candidate to vote:");
+
+            int ctr = 1;
+            foreach (Candidate c in samePosCandidate)
+                Console.WriteLine($"{ctr++}. {c.Name}");
+            Console.WriteLine($"{ctr}. ABSTAIN");
+
+            string temp = Console.ReadLine();
+            bool success = int.TryParse(temp, out int choice);
+            if (success && choice > 0 && choice < samePosCandidate.Count + 1)
+            {
+                if (chosenCandidate[indexOfPos[index]] == null)
+                {
+                    chosenCandidate[indexOfPos[index]] = samePosCandidate[choice - 1];
+                    index++;
+                    remainingVotes--;
+                }
+                else
+                {
+                    Console.WriteLine("You cannot vote the same candidate twice..");
+                    Console.ReadKey();
+                }
+            }
+            else if (success && choice > 0 && choice == samePosCandidate.Count + 1)
+                remainingVotes--;
+        }
+    }
+    
     public bool ShowVoteSummary()
     {
         Console.Clear();
